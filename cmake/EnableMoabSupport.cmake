d30b283531b528fe40b9b800c5b9755c0e9e8c09
Alex Robinson <aprobinson@wisc.edu>
2014-01-17 15:45:54 -0600

    Made a small change to the build system. File globbing is now used to find all header and source files in the core/src directory.

--- cmake/EnableMoabSupport.cmake
+++ cmake/EnableMoabSupport.cmake
@@ -3,28 +3,33 @@
 # 1.) MOAB - stores the Moab library name
 MACRO(ENABLE_MOAB_SUPPORT)
 
-  # Use the user supplied prefix to find the MOAB libraries and include dirs.
-  SET(MOAB_LIBRARY_DIRS ${MOAB_PREFIX}/lib)
-  SET(MOAB_INCLUDE_DIRS ${MOAB_PREFIX}/include)
+  # Add the user supplied Moab prefix to help find Moab
+  SET(CMAKE_PREFIX_PATH ${MOAB_PREFIX}/lib ${CMAKE_PREFIX_PATH})
 
-  # Find the MOAB library
-  FIND_LIBRARY(MOAB libMOAB.a ${MOAB_LIBRARY_DIRS})
+  # Find the Moab package available on this system
+  FIND_PACKAGE(MOAB REQUIRED)
+
+  # Use the user supplied prefix to find the Moab libraries and include dirs.
+  # SET(MOAB_INCLUDE_DIRS ${MOAB_PREFIX}/include)
+  SET(MOAB_LIBRARY_DIRS ${MOAB_PREFIX}/lib)
+  FIND_LIBRARY(MOAB MOAB ${MOAB_LIBRARY_DIRS})
   
   IF(${MOAB} MATCHES NOTFOUND)
-    MESSAGE(FATAL_ERROR "The MOAB library libMOAB.a could not be found.")
+    MESSAGE(FATAL_ERROR "The moab library could not be found.")
   ENDIF()
-    
-  # Set the MOAB library
-  # NOTE: There is a bug in the hdf51.8.12 static library build - the -ldl flag
-  #       is not passed to the linker during library creation. It must therefore
-  #       be passed here. This should hopefully be fixed in the next version of 
-  #       hdf5 that is released.
-  SET(MOAB ${MOAB} -lhdf5 -ldl)
-  
-  # Set the include paths for MOAB
+
+  FIND_LIBRARY(DAGMC dagmc ${MOAB_LIBRARY_DIRS})
+
+  IF(${DAGMC} MATCHES NOTFOUND)
+    MESSAGE(FATAL_ERROR "The dagmc library could not be found.")
+  ENDIF()
+
+  SET(MOAB ${DAGMC} ${MOAB} ${MOAB_LIBRARIES})
+
+  # Set the include paths for Moab
   INCLUDE_DIRECTORIES(${MOAB_INCLUDE_DIRS})
 
-  # Set the link paths for MOAB
+  # Set the link paths for Moab
   LINK_DIRECTORIES(${MOAB_LIBRARY_DIRS})
 
   # Echo the Moab details if a verbose configure was requested
@@ -33,7 +38,7 @@ MACRO(ENABLE_MOAB_SUPPORT)
     MESSAGE(" MOAB_PREFIX = ${MOAB_PREFIX}")
     MESSAGE(" MOAB_INCLUDE_DIRS = ${MOAB_INCLUDE_DIRS}")
     MESSAGE(" MOAB_LIBRARY_DIRS = ${MOAB_LIBRARY_DIRS}")
-    MESSAGE(" MOAB_LIBRARIES = ${MOAB}")
+    MESSAGE(" MOAB_LIBRARY = ${MOAB}")
     MESSAGE("End of MOAB details\n")
   ENDIF()
 
